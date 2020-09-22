adc75bfd5806e7d07bcaedc397f9cbc24a1c856f
Colin Henson <31398339+chenson399@users.noreply.github.com>
2020-09-22 19:01:19 -0400

    Updated Test Files
    2 files updated and 2 new test files added

--- src/phase1/python/HoughCirclesTest.py
+++ src/phase1/python/HoughCirclesTest.py
@@ -1,3 +1,7 @@
+# Colin Henson and Bryce George
+# CS 4800 Computer Graphics
+# Finds bowls in a frame using houghTransform from OpenCV
+
 import sys
 import cv2 as cv
 import numpy as np
@@ -7,10 +11,13 @@ import time
 def main(argv):
     # Sets a timer to start
     start = time.time()
+    # Frame from the video
     default_file = "frame2657.jpg"
 
+    # Opens the image and stores it in src variable
     src = cv.imread(default_file, cv.IMREAD_COLOR)
 
+    # Checks to see if frame was found
     if src is None:
         print("Error")
         return -1
@@ -18,36 +25,42 @@ def main(argv):
     gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
     # Blurs the image
     gray = cv.medianBlur(gray, 5)
-    # gray = cv.GaussianBlur(gray, (7, 7),sigmaX=1.5,sigmaY=1.5)
+
     # Sets rows to the shape of the grayscaled image
     rows = gray.shape[0]
     # rows/20, param1=150, param2=12, minRadius=1, maxRadius=30
     # Checks for circles, the parameters can be changed to change sensitivity of algorithm
+    # Link to houghcircles documentation below
+    # https://docs.opencv.org/master/dd/d1a/group__imgproc__feature.html#ga47849c3be0d0406ad3ca45db65a25d2d
     circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 20, param1=150, param2=12, minRadius=1, maxRadius=30)
     count = 0
-
+    # Checks to make sure it found circles
     if circles is not None:
         # Rounds the numbers in the circles list
+        # cv.circle cannot take floats
         circles = np.uint16(np.around(circles))
 
+        # Visualizes found circles and their centers on image
         for i in circles[0, :]:
             # Finds the center of the circles detected
-            count += 1
             center = (i[0], i[1])
             cv.circle(src, center, 1, (0, 100, 100), 3)
 
             # Puts a circle around the bowl
             radius = i[2]
             cv.circle(src, center, radius, (255, 0, 255), 3)
+            count += 1
 
     # Shows the results
     cv.imshow("detected circles", src)
     finish = time.time()
+    # Calculates total time
     total = finish - start
     # Prints number of circles found and the time it took to find them
     # There are 46 bowls in this frame
-    print(count)
-    print(total)
+    print("Number of bowls found: {}".format(count))
+    print("Total execution time: {}".format(total))
+    # Waits until user is done
     cv.waitKey(0)
     return 0
 
--- src/phase1/python/MultiTrackerTest.py
+++ src/phase1/python/MultiTrackerTest.py
@@ -1,74 +1,104 @@
+# Colin Henson and Bryce George
+# CS 4800 Computer Graphics
+# Tests different tracking algorithms using OpenCV MultiTracker
+
 from __future__ import print_function
 import sys
 import cv2
 from random import randint
 import time
 
+
 # Test different trackers
 def createTrackerByName(trackerType):
     Tracker = None
+    # Kernelized Correlation Filter
     if trackerType == "KCF":
         Tracker = cv2.TrackerKCF_create()
-    elif trackerType == "GOTurn":
+    # General Object using Regression Networks
+    elif trackerType == "GOTURN":
         Tracker = cv2.TrackerGOTURN_create()
+    # Discriminative Correlation Filter Tracker with Channel and Spacial Reliability
     elif trackerType == "CSRT":
         Tracker = cv2.TrackerCSRT_create()
+    # Defaults to None
     else:
         Tracker = None
 
     return Tracker
-# Name of video
-videoPath = "secondtestvideo.avi"
-# Video capture video
+
+
+# Name of video/Path of video
+videoPath = "video.avi"
+
+# Opens the video to be read
 cap = cv2.VideoCapture(videoPath)
 
+# Reads the video into memory
 success, frame = cap.read()
+
 # Checks to see if it can read video
 if not success:
     print('Failed to read video')
     sys.exit(1)
-# Initialize lists
+# Initialize lists for bounding boxes and random colors
 bboxes = []
 colors = []
-# ROI while loop to find bounded boxes
+
+# User selects region of interest
+# 1. Select target object with mouse
+# 2. Press space
+# 3. To select more boxes, press space again
+# 4. When done, press q
 while True:
+    # Creates the object selection window
     bbox = cv2.selectROI("MultiTracker", frame)
+    # Adds box coordinates to list
     bboxes.append(bbox)
-    # Make boxes random colors
+    # Picks random colors for bounding boxes
     colors.append((randint(0, 255), randint(0, 255), randint(0, 255)))
     print("Press q to quit selecting boxes and start tracking")
     print("Press any other key to select new object")
+    # Waits until user presses q
     k = cv2.waitKey(0) & 0xFF
     if k == 113:
         break
 
+# Purely for testing purposes
+# Displays coordinates of boxes in console
 print('Selected Bounding Boxes {}'.format(bboxes))
-# Use CSRT, works best
+# Defines which tracking algorithm you want to use
+# Recommend CSRT, works best in testing
 trackerType = "CSRT"
+# Creates the multi tracker object
 multiTracker = cv2.MultiTracker_create()
 
+# Iterates through bounding box list
 for bbox in bboxes:
-    # Create multitracker using tracking algorithm above
+    # Adds a tracker for each bounding box
     multiTracker.add(createTrackerByName(trackerType), frame, bbox)
-# While loop to tracks objects
+# While loop to tracks objects in video
 while cap.isOpened():
+    # Makes sure video has been read
     success, frame = cap.read()
     if not success:
 
         break
-        # Time delay
-    time.sleep(.25)
-    # Update tracking on frame
+    # Time delay between frame rendering
+    # Without time delay, frames are rendered as fast as processed
+    time.sleep(.1)
+    # Update tracking on video frame
     success, boxes = multiTracker.update(frame)
-    # Creates a new box around the tracked element
+    # Creates a new box around the tracked element for each frame
     for i, newbox in enumerate(boxes):
         p1 = (int(newbox[0]), int(newbox[1]))
         p2 = (int(newbox[0] + newbox[2]), int(newbox[1] + newbox[3]))
         cv2.rectangle(frame, p1, p2, colors[i], 2, 1)
     # Shows next tracked frame
     cv2.imshow('MultiTracker', frame)
-    
-    if cv2.waitKey(1) & 0xFF == 27:
+
+    # Waits until video is done or user pressed Esc
+    if cv2.waitKey(1) & 0xFF == 27: 
         break
 
 
